a632da68dbfc75286910ae5cc92deff31863bb86
Katarzyna Brejna <kb.brejna@gmail.com>
2023-02-21 15:24:27 +0100

    Lesson Objects

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ let C = 2;
 let M = 2;
 let E = M * C * C;
 console.log(E);
+
+let person1 = {
+    name: "Katarzyna",
+    lastName: "Brejna"
+};
+
+let person2 = {
+    name: "Jan",
+    lastName: "Kowalski"
+};
+
+console.log(person2.lastName + " " + person2.name);
