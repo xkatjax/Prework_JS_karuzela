84e1d41e4f7718ba0929e204713731227dab5c97
Katarzyna Brejna <kb.brejna@gmail.com>
2023-02-21 16:44:26 +0100

    Lesson - Functions

---
+++ example.js
@@ -0,0 +1,7 @@
+let myFirstFunction = (name, greeting) =>
+{
+    console.log( greeting + "! Mam na imię " + name + "!");
+};
+
+myFirstFunction('Kasia', "Hejka");
+myFirstFunction('Olga', "Witam");
