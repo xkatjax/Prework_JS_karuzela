49bc996c93b2eabcdac248e33c911961778727df
Katarzyna Brejna <kb.brejna@gmail.com>
2023-02-21 16:09:43 +0100

    Add variables to future slider

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-/* Zadanie - ZMIENNE */
+/* Zadanie - ZMIENNE 
 let C = 2;
 let M = 2;
 let E = M * C * C;
 console.log(E);
+*/
 
-/* Lekcja - OBIEKTY */
-let person1 = {
-    name: "Katarzyna",
-    lastName: "Brejna"
-};
-
-let person2 = {
-    name: "Jan",
-    lastName: "Kowalski"
-};
-
-console.log(person2.lastName + " " + person2.name);
-
-/* Zadanie - OBIEKTY */
+/* Zadanie - OBIEKTY 
 let car1 = {
     mark: "Fiat",
     model: "Punto",
@@ -32,3 +20,17 @@ let car2 = {
 
 console.log(car1.mark + ' ' + car1.model + ' ' + car1.productionYear);
 console.log(car2.mark + ' ' + car2.model + ' ' + car2.productionYear);
+*/
+
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right');
+
+let dot1 = document.querySelector('#dot1');
+let dot2 = document.querySelector('#dot2');
+let dot3 = document.querySelector('#dot3');
+
+let slide1 = document.querySelector('#slide1');
+let slide2 = document.querySelector('#slide2');
+let salajd3 = document.querySelector('#slide3');
+
+dot2.addEventListener("click");
