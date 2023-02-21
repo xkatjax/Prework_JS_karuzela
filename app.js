32ff97a99a2e77ebead87f48b0408c5e8fcbf8ce
Katarzyna Brejna <kb.brejna@gmail.com>
2023-02-21 15:30:04 +0100

    Add exercise - Objects

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let M = 2;
 let E = M * C * C;
 console.log(E);
 
+/* Lekcja - OBIEKTY */
 let person1 = {
     name: "Katarzyna",
     lastName: "Brejna"
@@ -15,3 +16,19 @@ let person2 = {
 };
 
 console.log(person2.lastName + " " + person2.name);
+
+/* Zadanie - OBIEKTY */
+let car1 = {
+    mark: "Fiat",
+    model: "Punto",
+    productionYear: 1999
+};
+
+let car2 = {
+    mark: "BMW",
+    model: "E90",
+    productionYear: 2013
+};
+
+console.log(car1.mark + ' ' + car1.model + ' ' + car1.productionYear);
+console.log(car2.mark + ' ' + car2.model + ' ' + car2.productionYear);
