b0a2da2460f336bd9e9e70e056a2bff986965302
Katarzyna Brejna <kb.brejna@gmail.com>
2023-02-21 16:56:20 +0100

    Add exercise - Functions

--- example.js
+++ example.js
@@ -5,3 +5,31 @@ let myFirstFunction = (name, greeting) =>
 
 myFirstFunction('Kasia', "Hejka");
 myFirstFunction('Olga', "Witam");
+
+/* Zadanie - FUNKCJE */
+// Zadanie 1
+let multiplicationByTwo = (number) =>
+{
+    console.log(number * 2);
+}
+
+multiplicationByTwo(4);
+
+//Zadanie 2
+let anyMultiplication = (number1, number2) =>
+{
+    console.log(number1 * number2);
+}
+
+anyMultiplication(5, 7);
+anyMultiplication(2, 6);
+
+//Zadanie 3
+let exponentiation = (number) =>
+{
+    console.log(number * number);
+}
+
+exponentiation(2);
+exponentiation(5);
+exponentiation(11);
